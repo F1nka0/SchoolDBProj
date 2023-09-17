46af924734f54c636bc4cb60c93659c5f7827fcf
F1nk40 <riainar@bk.ru>
2023-09-17 13:53:01 +0300

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolDB
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SchoolDBEntities a = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            a = new SchoolDBEntities();
+            grid.Loaded += InitList;
+            Tables.SelectionChanged += OnTableSelected;
+            
+        }
+        private async void InitList(object sender,EventArgs e) {
+            IEnumerable<Table> teachers = await Task.Run(() => { return a.Database.SqlQuery<Table>("SELECT name FROM sys.tables"); });
+            Tables.ItemsSource = teachers.Select(it=>it.Name).ToList();
+
+        }
+        private async void OnTableSelected(object sender, SelectionChangedEventArgs e) {
+
+            IEnumerable<object> tables = null;
+            var b = Dispatcher.Invoke(() => { return Tables.SelectedItem; });
+            switch (debugText.Text = Dispatcher.Invoke(() => { return Tables.SelectedItem; }).ToString())
+            {
+                case "Mark": tables = await (Task.Run(() => { return a.Database.SqlQuery<Mark>($"select * from {b}"); })); break;
+                case "Palor": tables = await (Task.Run(() => { return a.Database.SqlQuery<Palor>($"select * from {b}"); })); break;
+                case "Pupil": tables = await (Task.Run(() => { return a.Database.SqlQuery<Pupil>($"select * from {b}"); })); break;
+                case "ReportCard": tables = await (Task.Run(() => { return a.Database.SqlQuery<ReportCard>($"select * from {b}"); })); break;
+                case "Teacher": tables = await (Task.Run(() => { return a.Database.SqlQuery<Teacher>($"select * from {b}"); })); break;
+            }
+            SelectedData.ItemsSource = tables.ToList();
+        }
+    }
+    public class DataReport<T> { 
+        
+    }
+}
+/*
+            List<Table> selected = (await Task.Run(() => { return a.Database.SqlQuery<Table>($"select * from {e.AddedItems[0].ToString()}"); })).ToList();
+            SelectedData.ItemsSource = selected.Select(it=>it).ToList();
+ */
